a5ccb8498699eb59ef52b0f4e1886f5631bca25a
Eric <klingene@gmail.com>
2026-05-02 22:42:52 -0500

    enable foreign key constraints

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,6 @@
 
 import sqlite3
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
@@ -12,6 +12,12 @@ load_dotenv(".env") #Is relative to where python runs from
 engine = create_engine(
     os.getenv("DATABASE_URL"), connect_args={"check_same_thread": False}
 )
+
+@event.listens_for(engine, "connect")
+def enable_foreign_keys(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
